d85ab438172b3a753d1bfb564280d9fd24731fdf
ModuloM <modulom@modulom.net>
2016-03-03 13:52:42 +0100

    Fix build shell script
    Add no cache param to avoid cache use when docker images are built.
    Change commande to standard bash.

--- build.sh
+++ build.sh
@@ -2,9 +2,10 @@
 
 KODOKOJO_UI_VERSION="0.1.0"
 
-docker build -t="kodokojo/kodokojo-ui:builder" docker/builder/
+docker build --no-cache -t="kodokojo/kodokojo-ui:builder" docker/builder/
 docker run -v $(pwd):/src -v $(pwd)/docker/delivery/:/target -e "KODOKOJO_UI_VERSION=0.1.0" kodokojo/kodokojo-ui:builder
-pushd docker/delivery
+cd docker/delivery
+mkdir static
 tar zxvf kodokojo-ui-${KODOKOJO_UI_VERSION}.tar.gz -C static
-popd
-docker build -t="kodokojo/kodokojo-ui" docker/delivery
+cd ../..
+docker build --no-cache -t="kodokojo/kodokojo-ui" docker/delivery
