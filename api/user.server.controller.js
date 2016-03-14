13b13c7d7594191272a77b1526f39cca88e9841a
ModuloM <modulom@modulom.net>
2016-03-14 11:48:50 +0100

    Remove semi-colons

--- api/user.server.controller.js
+++ api/user.server.controller.js
@@ -5,7 +5,7 @@ export const initUser = (request, response) => {
   userRepository
     .initUser()
     .then((res) => {
-      return response.status(201).send(res);
+      return response.status(201).send(res)
     })
     .catch((err) =>{
       return res.status(500).send(err)
