b95d3a915aca8e7cf596c7a9e1849ada5e6c0987
ModuloM <modulom@modulom.net>
2016-03-18 15:45:48 +0100

    Clean imports

--- src/scripts/components/app/App.js
+++ src/scripts/components/app/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Link, browserHistory } from 'react-router'
+import { Link } from 'react-router'
 
 class App extends Component {
   render() {
