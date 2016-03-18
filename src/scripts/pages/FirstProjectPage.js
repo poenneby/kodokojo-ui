6aa9ec8219683d132d37721787b651e7f3352916
ModuloM <modulom@modulom.net>
2016-03-18 15:42:16 +0100

    Rename component FirstProject to Account

--- src/scripts/pages/FirstProjectPage.js
+++ src/scripts/pages/FirstProjectPage.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 
-import FirstProject from '../components/project/FirstProject'
+import Account from '../components/auth/Account'
 
 class FirstProjectPage extends Component {
   render() {
     return (
         <div style={{ width: '500px' }}>
-          <FirstProject />
+          <Account />
         </div>
     )
   }
