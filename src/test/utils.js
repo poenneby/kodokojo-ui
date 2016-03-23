6a2e8bd659d53567e2eed0c02f69220e0a0ee9b6
ModuloM <modulom@modulom.net>
2016-03-23 02:20:41 +0100

    Add url to jsdom in test utils

--- src/test/utils.js
+++ src/test/utils.js
@@ -1,28 +1,31 @@
 import { jsdom } from 'jsdom'
 
-global.document = jsdom('<!doctype html><html><body></body></html>')
+global.document = jsdom('<!doctype html><html><body></body></html>', {
+  url: 'http://localhost'
+})
 global.window = document.defaultView
 global.navigator = global.window.navigator
 
-//
-// function storageMock() {
-//   var storage = {};
-//
-//   return {
-//     setItem: (key, value) => storage[key] = value || '',
-//     getItem: (key) => storage[key] || null,
-//     removeItem: (key) => delete storage[key],
-//     length: () => Object.keys(storage).length,
-//     key: (i) => {
-//       var keys = Object.keys(storage)
-//       return keys[i] || null
-//     }
-//   }
-// }
-//
-// // mock the localStorage
-// global.window.localStorage = storageMock()
-// global.window.sessionStorage = storageMock()
-//
-// // mock the sessionStorage
-// global.window.sessionStorage = storageMock()
+/**
+ * Mocking local and session Storage
+ */
+function storageMock() {
+  var storage = {};
+
+  return {
+    setItem: (key, value) => storage[key] = value || '',
+    getItem: (key) => storage[key] || null,
+    removeItem: (key) => delete storage[key],
+    length: () => Object.keys(storage).length,
+    key: (i) => {
+      var keys = Object.keys(storage)
+      return keys[i] || null
+    }
+  }
+}
+
+// mock the localStorage
+global.window.localStorage = storageMock()
+
+// mock the sessionStorage
+global.window.sessionStorage = storageMock()
