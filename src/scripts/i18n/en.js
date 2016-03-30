b5c889124b77aaf509b6caf9becf8195b95406b6
ModuloM <modulom@modulom.net>
2016-03-30 12:37:34 +0200

    Fix locale en content

--- src/scripts/i18n/en.js
+++ src/scripts/i18n/en.js
@@ -2,5 +2,5 @@ export default {
   'signin-email-label': 'Email',
   'signin-email-hint-label': 'your.email@domain.ext',
   'signin-button-label': 'Sign in',
-  'signin-login-link-label' : 'Déjà inscrit ? Identifiez-vous !'
+  'signin-login-link-label' : 'Already a user? Log in!'
 }
