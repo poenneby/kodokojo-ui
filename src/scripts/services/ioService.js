e8a35a9b61fcce33e7f806f7cbf5a4aef216507d
ModuloM <modulom@modulom.net>
2016-04-01 18:23:23 +0200

    Add io service to manage headers

---
+++ src/scripts/services/ioService.js
@@ -0,0 +1,28 @@
+import merge from 'lodash/merge'
+
+import { getToken } from './authService'
+
+const ioService = {}
+
+/**
+ * Return a headers object that merge basic, auth and param header object
+ *
+ * @param header {object} headers to add or overwrite
+ * @returns {object} headers
+ */
+ioService.getHeaders = (header) => {
+  const token = getToken()
+  return merge(
+    {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    header,
+    token ? { 'Authorization': `Basic ${token}` } : {}
+  )
+}
+
+// public API
+export const getHeaders = ioService.getHeaders
+
+export default ioService
---
+++ src/scripts/services/ioService.spec.js
@@ -0,0 +1,75 @@
+import chai, { expect } from 'chai'
+import sinon from 'sinon'
+import sinonChai from 'sinon-chai'
+chai.use(sinonChai)
+
+import ioService from './ioService'
+
+describe('io service', () => {
+
+  describe('getHeaders method', () => {
+    afterEach(() => {
+      ioService.__ResetDependency__('getToken')
+    })
+
+    it('should return basic headers', () => {
+      // Given
+      const getTokenSpy = sinon.stub().returns('')
+      ioService.__Rewire__('getToken', getTokenSpy)
+
+      // When
+      const returned = ioService.getHeaders()
+
+      // Then
+      expect(returned).to.be.deep.equal({
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      })
+      expect(getTokenSpy).to.have.callCount(1)
+      expect(getTokenSpy).to.have.calledWith()
+    })
+
+    it('should return headers with autorization set', () => {
+      // Given
+      const getTokenSpy = sinon.stub().returns('token')
+      ioService.__Rewire__('getToken', getTokenSpy)
+
+      // When
+      const returned = ioService.getHeaders()
+
+      // Then
+      expect(returned).to.be.deep.equal({
+        'Accept': 'application/json',
+        'Authorization': 'Basic token',
+        'Content-Type': 'application/json'
+      })
+      expect(getTokenSpy).to.have.callCount(1)
+      expect(getTokenSpy).to.have.calledWith()
+    })
+
+    it('should return headers merged wioth param', () => {
+      // Given
+      const getTokenSpy = sinon.stub().returns('token')
+      ioService.__Rewire__('getToken', getTokenSpy)
+      const customHeaders = {
+        Accept: 'application/xml',
+        Authorization: 'is not rewritable',
+        SuperCustomHeader: 'super'
+      }
+
+      // When
+      const returned = ioService.getHeaders(customHeaders)
+
+      // Then
+      expect(returned).to.be.deep.equal({
+        'Accept': 'application/xml',
+        'Authorization': 'Basic token',
+        'Content-Type': 'application/json',
+        'SuperCustomHeader': 'super'
+      })
+      expect(getTokenSpy).to.have.callCount(1)
+      expect(getTokenSpy).to.have.calledWith()
+    })
+  })
+
+})
