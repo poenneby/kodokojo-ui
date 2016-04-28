c69a79c465a568ec302204fc4eaa1e2d9b65d1c9
ModuloM <modulom@modulom.net>
2016-04-28 15:58:51 +0200

    Add missing project actions

---
+++ src/scripts/components/project/project.actions.js
@@ -0,0 +1,53 @@
+import { browserHistory } from 'react-router'
+import { CALL_API } from 'redux-api-middleware'
+
+import api from '../../commons/config'
+import { getHeaders } from '../../services/ioService'
+// import { mapProject } from '../../services/mappingService'
+import {
+  PROJECT_REQUEST,
+  PROJECT_SUCCESS,
+  PROJECT_FAILURE
+} from '../../commons/constants'
+
+
+export function requestProject(projectConfigId) {
+  return {
+    [CALL_API]: {
+      method: 'POST',
+      endpoint: `${window.location.protocol||'http:'}//${window.location.host||'localhost'}${api.project}/${projectConfigId}`,
+      headers: getHeaders(),
+      types: [
+        PROJECT_REQUEST,
+        {
+          type: PROJECT_SUCCESS,
+          payload: (action, state, res) => {
+            return res.text().then(id => {
+              return {
+                project: {
+                  id: id
+                }
+              }
+            })
+          }
+        },
+        PROJECT_FAILURE
+      ]
+    }
+  }
+}
+
+export function createProject(projectConfigId) {
+  return dispatch => {
+    return dispatch(requestProject(projectConfigId)
+    ).then(data => {
+      if (!data.error) {
+        browserHistory.push('/project')
+      } else {
+        throw new Error(data.payload.status)
+      }
+    }).catch(error => {
+      throw new Error(error.message)
+    })
+  }
+}
