7264c4a1c3dc147f614f726877cfe88a6e270374
ModuloM <modulom@modulom.net>
2016-05-04 14:32:25 +0200

    Add tests on exit values during build through build.sh and stop it if a step fail

--- build.sh
+++ build.sh
@@ -4,8 +4,22 @@ KODOKOJO_UI_VERSION="0.1.0"
 
 docker build --no-cache -t="kodokojo/kodokojo-ui:builder" docker/builder/
 docker run -v $(pwd):/src -v $(pwd)/docker/delivery/:/target -e "KODOKOJO_UI_VERSION=0.1.0" kodokojo/kodokojo-ui:builder
+containerId=$(docker ps -a -q -l)
+rc=$(docker inspect -f {{.State.ExitCode}} $containerId)
+docker rm $containerId
+if [ "$rc" != 0 ]; then
+  exit $rc
+fi
 cd docker/delivery
 mkdir static
 tar zxvf kodokojo-ui-${KODOKOJO_UI_VERSION}.tar.gz -C static
+rc=$?
+if [ "$rc" != 0 ]; then
+  exit $rc
+fi
 cd ../..
 docker build --no-cache -t="kodokojo/kodokojo-ui" docker/delivery
+rc=$?
+if [ "$rc" != 0 ]; then
+  exit $rc
+fi
--- docker/builder/build.sh
+++ docker/builder/build.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-npm i && npm test && npm run build:prod
-
+npm i
+rc=$?
+if [ "$rc" != 0 ]; then
+  exit $rc
+fi
+npm test
+rc=$?
+if [ "$rc" != 0 ]; then
+  exit $rc
+fi
+npm run build:prod
+rc=$?
+if [ "$rc" != 0 ]; then
+  exit $rc
+fi
 tar cvzf /target/kodokojo-ui-${KODOKOJO_UI_VERSION}.tar.gz -C /src/static/ .
-
+rc=$?
+if [ "$rc" != 0 ]; then
+  exit $rc
+fi
