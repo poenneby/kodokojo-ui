43b1de1b39dcd53e40c81c3b00cd5efb634ff6ee
ModuloM <modulom@modulom.net>
2016-05-20 18:21:22 +0200

    [ui] Layout - add layout ui component

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,6 +2,7 @@ import { configure } from '@kadira/storybook'
 
 function loadStories() {
   require('../src/scripts/components/_ui/appBar/AppBar.story')
+  require('../src/scripts/components/_ui/layout/Layout.story')
 }
 
 configure(loadStories, module)
