e8f025e73e093ee8def501a98a93fd38b876848f
ModuloM <modulom@modulom.net>
2016-05-21 00:49:21 +0200

    [ui] Avatar - add avater component

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,6 +2,7 @@ import { configure } from '@kadira/storybook'
 
 function loadStories() {
   require('../src/scripts/components/app/App.story')
+  require('../src/scripts/components/_ui/avatar/Avatar.story')
   require('../src/scripts/components/_ui/appBar/AppBar.story')
   require('../src/scripts/components/_ui/layout/Layout.story')
   require('../src/scripts/components/_ui/panel/Panel.story')
---
+++ src/scripts/components/_ui/avatar/Avatar.story.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Provider } from 'react-redux'
+import { storiesOf, action } from '@kadira/storybook'
+
+// contexte
+import configureStore from '../../../store/configureStore'
+
+// component to story
+import Avatar from './Avatar.component'
+
+const initialState = {}
+
+const store = configureStore(initialState)
+
+storiesOf('Avatar', module)
+  .add('with nav', () => (
+    <Avatar
+      store={store}
+    >
+      AL
+    </Avatar>
+  ))
