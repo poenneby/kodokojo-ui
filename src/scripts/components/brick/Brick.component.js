c3a625630120cb67e7e6cdafb3a0751b33c73463
ModuloM <modulom@modulom.net>
2016-05-26 14:57:46 +0200

    [feat] Brick - add brick component

---
+++ src/scripts/components/brick/Brick.component.js
@@ -0,0 +1,44 @@
+import React, { Component, PropTypes } from 'react'
+
+// Component
+import './brick.less'
+import { getBrickStatus } from '../../services/param.service'
+
+// TODO TU
+// Brick component
+export class Brick extends Component {
+
+  static propTypes = {
+    brick: PropTypes.object
+  }
+
+  render() {
+    const { brick } = this.props // eslint-disable-line no-shadow
+    const status = getBrickStatus(brick && brick.state ? brick.state : undefined)
+
+    return (
+      <div className="brick brick-item">
+        <div className="brick-column brick-type">
+          { brick ? brick.type : '-' }
+        </div>
+        <div className="brick-column brick-name">
+          { brick ? brick.name : '-' }
+        </div>
+        <div className="brick-column brick-state" >
+          <img className="brick-status" src={ status.image } />
+        </div>
+        <div className="brick-column brick-version">
+          { brick ? brick.version : '-' }
+        </div>
+        <div className="brick-column brick-link">
+          { brick && brick.url ?
+            <a href={ brick.url }>{ brick.url }</a> :
+            '-'
+          }
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Brick
---
+++ src/scripts/components/brick/Brick.story.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { Provider } from 'react-redux'
+import { storiesOf, action } from '@kadira/storybook'
+
+// component to story
+import Brick from './Brick.component'
+
+const brickRunning = {
+  type: 'type',
+  name: 'name',
+  state: 'RUNNING',
+  version: '2.1.2',
+  url: '#entity-type-linktobrick.kodokojo.io'
+}
+const brickStarting = {
+  type: 'type',
+  name: 'name',
+  state: 'STARTING',
+  version: '2.1.2',
+  url: ''
+}
+const brickFailure = {
+  type: 'type',
+  name: 'name',
+  state: 'ONFAILURE',
+  version: '2.1.2',
+  url: '#entity-type-linktobrick.kodokojo.io'
+}
+const brickDefault = {
+  type: 'type',
+  name: 'name',
+  state: undefined,
+  version: '2.1.2',
+  url: '#entity-type-linktobrick.kodokojo.io'
+}
+
+storiesOf('Brick', module)
+  .add('running', () => (
+    <Brick
+      brick={ brickRunning }
+    />
+  ))
+  .add('starting', () => (
+    <Brick
+      brick={ brickStarting }
+    />
+  ))
+  .add('failure', () => (
+    <Brick
+      brick={ brickFailure }
+    />
+  ))
+  .add('default', () => (
+    <Brick
+      brick={ brickDefault }
+    />
+  ))
