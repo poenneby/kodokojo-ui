f0dee19b864e6b561333db94367f8c80c1f6945b
ModuloM <modulom@modulom.net>
2016-05-27 07:13:47 +0200

    [refactor] Nav - refactor navdrawer into nav, drop react toolbox dependency

--- src/scripts/components/_ui/nav/Nav.component.story.js
+++ src/scripts/components/_ui/nav/Nav.component.story.js
@@ -6,13 +6,13 @@ import { storiesOf, action } from '@kadira/storybook'
 import configureStore from '../../../store/configureStore'
 
 // component to story
-import NavDrawer from './NavDrawer.component'
+import NavDrawer from './Nav.component.js'
 
 const initialState = {}
 
 const store = configureStore(initialState)
 
-storiesOf('NavDrawer', module)
+storiesOf('Nav', module)
   .add('pinned', () => (
     <Provider store={store}>
       <NavDrawer
