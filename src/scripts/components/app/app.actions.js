401276ff766a78e089610fffd809e6dde2a23d4d
ModuloM <modulom@modulom.net>
2016-05-27 07:18:35 +0200

    [refactor] App - refactor all app layout, nav and panel

--- src/scripts/components/app/app.actions.js
+++ src/scripts/components/app/app.actions.js
@@ -1,9 +1,26 @@
-import { SET_THEME, SET_LOCALE } from '../../commons/constants'
+import {
+  SET_THEME,
+  SET_LOCALE,
+  SET_NAV_VISIBILITY
+} from '../../commons/constants'
 
 export function setTheme(theme) {
-  return { type: SET_THEME, theme }
+  return {
+    type: SET_THEME,
+    theme
+  }
 }
 
 export function setLocale(locale) {
-  return { type: SET_LOCALE, locale }
+  return {
+    type: SET_LOCALE,
+    locale
+  }
+}
+
+export function setNavVisiblity(visibility) {
+  return {
+    type: SET_NAV_VISIBILITY,
+    visibility
+  }
 }
