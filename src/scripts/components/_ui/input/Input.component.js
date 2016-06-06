e2ed5bf8620c19d5d27b2685f71783b942003c10
ModuloM <modulom@modulom.net>
2016-06-06 17:30:50 +0200

    [refactor] Input - refactor react-toolbox input integration

--- src/scripts/components/_ui/input/Input.component.js
+++ src/scripts/components/_ui/input/Input.component.js
@@ -1,64 +1,17 @@
 import React, { Component, PropTypes } from 'react'
 
 // UI library component
-import { Input as ToolboxInput } from 'react-toolbox'
+import { Input as ToolboxInput } from 'react-toolbox/lib/input'
 
 // component
-import inputStyle from './input.scss'
+import inputTheme from './input.scss'
 
 /**
  * UI: Input component
  *
  */
-export class Input extends Component {
-
-  static propTypes = {
-    className: PropTypes.string,
-    disabled: PropTypes.bool,
-    error: PropTypes.string,
-    floating: PropTypes.bool,
-    hint: PropTypes.string,
-    icon: PropTypes.oneOfType([
-      PropTypes.element,
-      PropTypes.string
-    ]),
-    label: PropTypes.string,
-    maxLength: PropTypes.number,
-    multiline: PropTypes.bool,
-    onBlur: PropTypes.func,
-    onChange: PropTypes.func,
-    onFocus: PropTypes.func,
-    required: PropTypes.bool,
-    type: PropTypes.string,
-    value: PropTypes.any
-  }
-
-  render() {
-    const { className, disabled, error, floating, hint,
-      icon, label, maxLength, multiline, onBlur, onChange,
-      onFocus, required, type, value } = this.props // eslint-disable-line no-shadow
-    const inputClass = inputStyle.input
-
-    return (
-      <ToolboxInput
-        className={ className ? `${inputClass} ${className}` : `${inputClass}` }
-        disabled={ disabled }
-        error={ error }
-        floating={ floating }
-        hint={ hint }
-        icon={ icon }
-        label={ label }
-        maxLength={ maxLength }
-        multiline={ multiline }
-        onBlur={ onBlur }
-        onChange={ onChange }
-        onFocus={ onFocus }
-        required={ required }
-        type={ type }
-        value={ value }
-      />
-    )
-  }
-}
+const Input = (props) => (
+  <ToolboxInput{...props} theme={ inputTheme } />
+)
 
 export default Input
