2fc0aa8c42f0d797e8a2cc042c87ca5e09555080
ModuloM <modulom@modulom.net>
2016-06-23 15:19:07 +0200

    [refactor] FirstProjectPage ProjectConfigForm - refactor to implement new design

--- src/scripts/pages/FirstProject.page.js
+++ src/scripts/pages/FirstProject.page.js
@@ -1,45 +1,81 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
+import { compose } from 'redux'
 import { intlShape, injectIntl, FormattedMessage } from 'react-intl'
 
-// UI
-import Paper from 'material-ui/Paper'
-
 // Component
-import { centerPaper } from '../../styles/commons'
+import '../../styles/_commons.less'
+import utilsTheme from '../../styles/_utils.scss'
+import Page from '../components/_ui/page/Page.component'
+import Card from '../components/_ui/card/Card.component'
 import Account from '../components/auth/Account.component.js'
 import ProjectConfigForm from '../components/projectConfig/ProjectConfigForm.component'
+import { setNavVisibility } from '../components/app/app.actions'
+import { getBricks } from '../components/brick/brick.actions'
 
-class FirstProjectPage extends Component {
+export class FirstProjectPage extends Component {
 
   static propTypes = {
-    intl: intlShape.isRequired
+    getBricks: PropTypes.func.isRequired,
+    intl: intlShape.isRequired,
+    setNavVisibility: PropTypes.func.isRequired
+  }
+
+  componentWillMount() {
+    const { getBricks } = this.props // eslint-disable-line no-shadow
+
+    this.initNav()
+
+    // refresh available bricks
+    getBricks()
   }
 
   componentWillUnmount() {
     // TODO dispatch action that clean auth from sensitive infos (password, ssh keys)
   }
 
+  initNav = () => {
+    const { setNavVisibility } = this.props // eslint-disable-line no-shadow
+
+    setNavVisibility(false)
+  }
+
   render() {
     return (
-      <div>
-        <Paper
-          className="center"
-          style={ centerPaper }
-          zDepth={1}
-        >
-          <Account />
-        </Paper>
-        <Paper
-          className="center"
-          style={ centerPaper }
-          zDepth={1}
-        >
-          <FormattedMessage id={'project-config-intro-text'} />
-          <ProjectConfigForm/>
-        </Paper>
+    <Page>
+      <h1 className={ utilsTheme['secondary-color--1'] }>
+        <FormattedMessage id={'project-create-label'} />
+      </h1>
+      <div className="paragraph">
+        <div style={{ display: 'flex', flexFlow: 'row wrap', marginBottom: '10px' }}>
+          <Card>
+            <Account />
+          </Card>
+        </div>
+        <ProjectConfigForm />
       </div>
+    </Page>
     )
   }
 }
 
-export default injectIntl(FirstProjectPage)
+// FirstProjectPage container
+const mapStateProps = (state, ownProps) => (
+  {
+    location: ownProps.location,
+    bricks: state.bricks
+  }
+)
+
+const FirstProjectContainer = compose(
+  connect(
+    mapStateProps,
+    {
+      getBricks,
+      setNavVisibility
+    }
+  ),
+  injectIntl
+)(FirstProjectPage)
+
+export default FirstProjectContainer
