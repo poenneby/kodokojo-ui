730fa05d14eedb57989d9e8b07424ac46823466b
ModuloM <modulom@modulom.net>
2016-08-22 16:52:38 +0200

    [project] build docker ui image version based on package

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# TODO get version from other file, automatically tag when build (replace package.json one)
-KODOKOJO_UI_VERSION="0.1.0"
+# save package version in env variable
+export KODOKOJO_UI_VERSION=$(npm version --json | jq -r '."kodokojo-ui"')
 
 docker build --no-cache -t="kodokojo/kodokojo-ui:builder" docker/builder/
-containerId=$(docker create -e "KODOKOJO_UI_VERSION=0.1.0" kodokojo/kodokojo-ui:builder)
+containerId=$(docker create -e "KODOKOJO_UI_VERSION=${KODOKOJO_UI_VERSION}" kodokojo/kodokojo-ui:builder)
 docker cp $(pwd)/. ${containerId}:/src/
 docker start -a $containerId
 docker cp ${containerId}:/target/ $(pwd)/docker/delivery/
