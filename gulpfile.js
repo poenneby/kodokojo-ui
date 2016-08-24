34dce3bf702f2d3febb567c266c9da29cfdc5bad
ModuloM <modulom@modulom.net>
2016-08-24 14:11:43 +0200

    [project] add gulp and npm task to add license in source files headers

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var fs = require('fs')
+var gulp = require('gulp')
+var changed = require('gulp-changed')
+var license = require('gulp-header-license')
+
+var files = [
+  './webpack*.js',
+  './server*.js',
+  './src/**/*.js',
+  './src/**/*.less',
+  './src/**/*.scss',
+  './config/**/*.js',
+  './api/**/*.js'
+]
+
+var year = (new Date()).getFullYear()
+
+gulp.task('license', function () {
+  return gulp.src(files, { base: './' })
+    .pipe(changed('./**/*'))
+    .pipe(license(fs.readFileSync('LICENSE', 'utf8'), { year }))
+    .pipe(gulp.dest(function (file) {
+      return file.base
+    }))
+})
