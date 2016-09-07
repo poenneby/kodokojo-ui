f9e21669d9a4a3ccf8f1c471400e1ceeb8a0c8ec
ModuloM <modulom@modulom.net>
2016-09-07 10:25:19 +0200

    [clean] loginReducer - delete reducer and specs, duplicate of auth reducer

--- src/scripts/commons/reducers.js
+++ src/scripts/commons/reducers.js
@@ -22,7 +22,6 @@ import { reducer as formReducer } from 'redux-form'
 
 import auth from '../components/auth/auth.reducer.js'
 import bricks from '../components/brick/brick.reducer.js'
-import login from '../components/login/login.reducer'
 import menu from '../components/menu/menu.reducer'
 import prefs from '../components/app/app.reducer'
 import projectConfig from '../components/projectConfig/projectConfig.reducer'
@@ -32,7 +31,6 @@ import users from '../components/user/user.reducer'
 const rootReducer = combineReducers({
   auth,
   bricks,
-  login,
   menu,
   prefs,
   projectConfig,
